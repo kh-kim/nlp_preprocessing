cabb65fccb1f2bcab8d579778bec8940a4dfd980
Ki Hyun Kim <pointzz.ki@gmail.com>
2018-04-21 22:21:58 +0900

    add

--- detokenizer.py
+++ detokenizer.py
@@ -3,6 +3,6 @@ import sys
 if __name__ == "__main__":
     for line in sys.stdin:
         if line.strip() != "":
-            line = line.strip().replace(' ', '').replace('▁▁', ' ').replace('▁', '')
+            line = line.strip().replace(' ', '').replace('▁▁', ' ').replace('▁', '').strip()
 
             sys.stdout.write(line + '\n')
