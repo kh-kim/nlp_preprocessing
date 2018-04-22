d4c8695b1360c340416ded8edb381d3a92221732
Ki Hyun Kim <pointzz.ki@gmail.com>
2018-04-22 19:24:34 +0900

    minor

--- align.ted.sh
+++ align.ted.sh
@@ -8,10 +8,10 @@ if [ -f $FN-$SRC_LANG.txt ]
 then
     if [ -f $FN-$TGT_LANG.txt ]
     then
-        cat $FN-$SRC_LANG.txt | python ./combine_line.py > ./tmp/tmp.src.txt
+        cat $FN-$SRC_LANG.txt | python ./combine_line.py | python refine.py regex.txt > ./tmp/tmp.src.txt
         python ./tokenizer.py < ./tmp/tmp.src.txt > ./tmp/tmp.src.tok.txt
 
-        cat $FN-$TGT_LANG.txt | python ./combine_line.py > ./tmp/tmp.tgt.txt
+        cat $FN-$TGT_LANG.txt | python ./combine_line.py | python refine.py regex.txt > ./tmp/tmp.tgt.txt
         mecab --input-buffer-size=30000 -O wakati < ./tmp/tmp.tgt.txt > ./tmp/tmp.tgt.tok.txt
 
         python align.py --src ./tmp/tmp.src.txt --tgt ./tmp/tmp.tgt.txt --dict $DICT_FN --src_ref ./tmp/tmp.src.tok.txt --tgt_ref ./tmp/tmp.tgt.tok.txt
